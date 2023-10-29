da5df85254d80f4d92d8fab1075348a2e719a49d
gkatanacio <gkatanacio.swe@gmail.com>
2023-10-29 21:42:35 +0800

    Rename sample env var

--- src/handlers/hello.ts
+++ src/handlers/hello.ts
@@ -7,7 +7,7 @@ const greetingService = new GreetingService('!');
 module.exports.handle = async (event: APIGatewayProxyEvent, context: Context) => {
   const result = {
     requestId: context.awsRequestId,
-    message: greetingService.greet(process.env.HELLO_WHO as string),
+    message: greetingService.greet(process.env.GREET_WHO as string),
   };
 
   return {
