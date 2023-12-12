4207ea05da2342f3e6a15b2525eaad720db5b755
gkatanacio <gkatanacio.swe@gmail.com>
2023-12-12 21:30:04 +0800

    Rename parameter containing http request and add logging

--- src/handlers/hello.ts
+++ src/handlers/hello.ts
@@ -4,7 +4,9 @@ import GreetingService from '../services/greeting-service';
 
 const greetingService = new GreetingService('!');
 
-module.exports.handle = async (event: APIGatewayProxyEvent, context: Context) => {
+module.exports.handle = async (request: APIGatewayProxyEvent, context: Context) => {
+  console.log(request);
+
   const result = {
     requestId: context.awsRequestId,
     message: greetingService.greet(process.env.GREET_WHO as string),
